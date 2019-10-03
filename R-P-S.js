53d4b7195c5053bbb35da5e594f19e1369f739b9
samSterns <sam.g.sterns@gmail.com>
2019-10-02 17:55:55 -0700

    end of day 3 with outline no test

---
+++ R-P-S.js
@@ -0,0 +1,45 @@
+//fetch DOM elements
+
+
+//set intitial stater 
+
+//define DOM utility function
+
+
+// const generateRandomInt => {
+    
+// }
+
+const rockValue = 0;
+constpaperValue= 1
+const
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+console.log(getRandomInt(3));
+
+const getRandomThrow = () => {
+    if (getZeroOneTwo === 0) {
+        return rock;
+    } else if (getZeroOneTwo === 1) {
+        return 'paper';
+    } else if (getZeroOneTwo === 2) {
+        return 'paper';}
+}
+
+//   // expected output: 0, 1 or 2
+// console.log(getRandomInt(2));
+// console.log(getRandomInt(1));
+// console.log(Math.random());
+  // expected output: a number between 0 and 1
+
+//const getRandomThrow = () => Math.round(Math.random()) {
+
+
+
+
+// set some initial state
+
+
+// define DOM utility function
